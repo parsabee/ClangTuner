b6c9d0b9a2fa2c1ae9d6831833c2aa01732a46d1
parsabee <35123665+parsabee@users.noreply.github.com>
2021-12-20 10:26:06 -0800

    [FuncOp] mr

--- mlir/lib/Dialect/Luminous/IR/Luminous.cpp
+++ mlir/lib/Dialect/Luminous/IR/Luminous.cpp
@@ -12,6 +12,7 @@
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/FunctionImplementation.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/TypeUtilities.h"
 #include "llvm/ADT/TypeSwitch.h"
@@ -72,6 +73,120 @@ static void print(OpAsmPrinter &p, LuminousModuleOp op) {
                                      {SymbolTable::getSymbolAttrName()});
   p.printRegion(op->getRegion(0), /*printEntryBlockArgs=*/false,
                 /*printBlockTerminators=*/false);
+}
+
+//===----------------------------------------------------------------------===//
+// LuminousFuncOp
+//===----------------------------------------------------------------------===//
+
+void LuminousFuncOp::build(OpBuilder &builder, OperationState &result,
+                           StringRef name, FunctionType type,
+                           ArrayRef<NamedAttribute> attrs) {
+  result.addAttribute(SymbolTable::getSymbolAttrName(),
+                      builder.getStringAttr(name));
+  result.addAttribute(getTypeAttrName(), TypeAttr::get(type));
+  result.addAttributes(attrs);
+  Region *body = result.addRegion();
+  Block *entryBlock = new Block;
+  entryBlock->addArguments(type.getInputs());
+  body->getBlocks().push_back(entryBlock);
+}
+
+static ParseResult parseLuminousFuncOp(OpAsmParser &parser,
+                                       OperationState &result) {
+  SmallVector<OpAsmParser::OperandType, 8> entryArgs;
+  SmallVector<NamedAttrList, 1> argAttrs;
+  SmallVector<NamedAttrList, 1> resultAttrs;
+  SmallVector<Type, 8> argTypes;
+  SmallVector<Type, 4> resultTypes;
+  bool isVariadic;
+
+  // Parse the function name.
+  StringAttr nameAttr;
+  if (parser.parseSymbolName(nameAttr, ::mlir::SymbolTable::getSymbolAttrName(),
+                             result.attributes))
+    return failure();
+
+  auto signatureLocation = parser.getCurrentLocation();
+  if (failed(function_like_impl::parseFunctionSignature(
+          parser, /*allowVariadic=*/false, entryArgs, argTypes, argAttrs,
+          isVariadic, resultTypes, resultAttrs)))
+    return failure();
+
+  if (entryArgs.empty() && !argTypes.empty())
+    return parser.emitError(signatureLocation)
+           << "requires named arguments";
+
+  if (!resultAttrs.empty() || !resultTypes.empty())
+    return parser.emitError(signatureLocation)
+           << "does not expect return type";
+
+  // Construct the function type. More types will be added to the region, but
+  // not to the function type.
+  Builder &builder = parser.getBuilder();
+  auto type = builder.getFunctionType(argTypes, resultTypes);
+  result.addAttribute(LuminousFuncOp::getTypeAttrName(), TypeAttr::get(type));
+
+  // Parse attributes.
+  if (failed(parser.parseOptionalAttrDictWithKeyword(result.attributes)))
+    return failure();
+  function_like_impl::addArgAndResultAttrs(builder, result, argAttrs,
+                                           resultAttrs);
+
+  // Parse the region. If no argument names were provided, take all names
+  // (including those of attributions) from the entry block.
+  auto *body = result.addRegion();
+  return parser.parseRegion(*body, entryArgs, argTypes);
+}
+
+static void printLuminousFuncOp(OpAsmPrinter &p, LuminousFuncOp op) {
+  p << ' ';
+  p.printSymbolName(op.getName());
+
+  FunctionType type = op.getType();
+  function_like_impl::printFunctionSignature(
+      p, op.getOperation(), type.getInputs(),
+      /*isVariadic=*/false, type.getResults());
+
+  function_like_impl::printFunctionAttributes(
+      p, op.getOperation(), type.getNumInputs(), type.getNumResults(), {});
+  p.printRegion(op.getBody(), /*printEntryBlockArgs=*/false);
+}
+
+LogicalResult LuminousFuncOp::verifyType() {
+  Type type = getTypeAttr().getValue();
+  if (!type.isa<FunctionType>())
+    return emitOpError("requires '" + getTypeAttrName() +
+                       "' attribute of function type");
+
+  if (getType().getNumResults() != 0)
+    return emitOpError() << "expected no return type";
+
+  return success();
+}
+
+LogicalResult LuminousFuncOp::verifyBody() {
+  unsigned numFuncArguments = getNumArguments();
+  unsigned numBlockArguments = front().getNumArguments();
+  if (numBlockArguments < numFuncArguments)
+    return emitOpError() << "expected at least " << numFuncArguments
+                         << " arguments to body region";
+
+  ArrayRef<Type> funcArgTypes = getType().getInputs();
+  for (unsigned i = 0; i < numFuncArguments; ++i) {
+    Type blockArgType = front().getArgument(i).getType();
+    if (funcArgTypes[i] != blockArgType)
+      return emitOpError() << "expected body region argument #" << i
+                           << " to be of type " << funcArgTypes[i] << ", got "
+                           << blockArgType;
+  }
+  return success();
+}
+
+static LogicalResult verify(LuminousFuncOp op) {
+  if (failed(op.verifyBody()) || failed(op.verifyType()))
+    return failure();
+  return success();
 }
 
 #define GET_OP_CLASSES
--- mlir/test/Dialect/Luminous/ops.mlir
+++ mlir/test/Dialect/Luminous/ops.mlir
@@ -3,5 +3,8 @@
 module attributes {luminous.container_module} {
   // CHECK-LABEL: luminous.module @kernels
   luminous.module @kernels {
+    luminous.func @kernel (%arg0: memref<1024xf32>, %arg1: memref<2048xf32>) {
+      luminous.return
+    }
   }
 }
